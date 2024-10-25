9e4161b10dbbec9bf282b45818cba4447d7806e9
Chantal <chantalgomez@pursuit.org>
2024-10-25 12:22:44 -0400

    testing: cypress file updated to test a new user info for every login test.

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -4,14 +4,15 @@ describe('Sign up page', () => {
     cy.get('[data-cy="submit"]').click()
   })
 
-  it("Should a qrcode for 2fa once the user submits their data.", () => {
-    cy.visit("http://localhost:5173/");
-    cy.get('[data-cy="username-input"]').type("testUserr");
-    cy.get('[data-cy="email"]').type("testuserr@gmail.com");
-    cy.get('[data-cy="password"]').type("TheBigApple47!");
-    cy.get('[data-cy="submit"]').click();
-    cy.get('[data-cy="qrcode"]').should("exist");
-  });
+it("Should generate a QR code for 2FA once the user submits their data.", () => {
+  const uniqueEmail = `testuserr+${Date.now()}@gmail.com`;
+  cy.visit("http://localhost:5173/");
+  cy.get('[data-cy="username-input"]').type("testUserr");
+  cy.get('[data-cy="email"]').type(uniqueEmail);
+  cy.get('[data-cy="password"]').type("TheBigApple47!");
+  cy.get('[data-cy="submit"]').click();
+  cy.get('[data-cy="qrcode"]').should("exist");
+});
 })
 
 describe("Sign in page", () => {
